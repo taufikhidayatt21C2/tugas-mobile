e45ad4ed83d1356475e7b9f36ef6b0763371f5ce
Taufik hidayat <116345854+taufikhidayatt21C2@users.noreply.github.com>
2022-10-24 11:02:58 +0700

    first commit

---
+++ app/src/main/java/com/example/qrcodescanner_21c2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.qrcodescanner_21c2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
